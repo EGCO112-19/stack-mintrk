cb4d35116677a42fde8641878dcce8a912887671
mintrk <ruttiyaporn.kon@student.mahidol.edu>
2021-02-26 10:40:20 +0000

    use if case in main

--- main.c
+++ main.c
@@ -8,17 +8,67 @@ int main(int argc, char **argv){
   int i,N,j;
   
   Stack s;
-
-
- 
-  for(i=1;i<argc;i++){
-   
+  s.size = 0;
+  s.top = NULL;
+  char popvalue;
+  int check=0;
+  
+  
+  for(i=1;i<argc;i++){// each argument
+    //printf("%s\n",argv[i]);
+    //printf("%ld\n",strlen(argv[i]));
      for(j=0;j<strlen(argv[i]);j++){
        /* Use stack to help with the parentheses*/
+       if(argv[i][j] == '[' || argv[i][j] == '{'){
+          push(&s, argv[i][j]); 
+       }
+       else if (argv[i][j] == ']'){
+          if (s.size==0){
+            check = 1;
+            break;
+          }
+          popvalue = pop(&s);
+          if (popvalue != '['){
+            check = 1;
+            break;
+          }
+       }
+       else if (argv[i][j] == '}'){
+         if (s.size==0){
+           check = 1;
+            break;
+          }
+          popvalue = pop(&s);
+          if (popvalue != '{'){
+            check = 1;
+            break;
+          }
+       }
 
+       /*switch(argv[i][j]){
+         case '[':
+            break;
+         case '{':
+            push(&s, argv[i][j]);
+            break;
+         case ']':
+            
+            break;
+         case '}':
+            pop(&s);
+            break;
+       }*/
 
      }
-
+     if(s.size>0 || check != 0){
+         printf("%d ..\n",s.size);
+         
+         printf("The parenthses do not match for %s \n", argv[i]);
+         pop_all(&s);
+       }else{
+         printf("%d ..\n",s.size);
+         printf("The parenthses match successfully for %s \n", argv[i]);
+       }
 
   }
 
--- stack.h
+++ stack.h
@@ -9,6 +9,44 @@ typedef struct {
 
 typedef Stack * StackPtr;
 void push(StackPtr s, char value);
-char pop(Stack s);
-void pop_all(Stack s);
+char pop(StackPtr s);
+void pop_all(StackPtr s);
+
+void push(StackPtr s, char value){
+  printf("%c\n",value);
+  Node *new_node=(Node *)malloc(sizeof(Node));
+  if(new_node){
+    new_node-> data = value;
+    new_node-> nextPtr = s->top;
+    s->top = new_node;
+    s->size += 1;
+    printf("%d ..\n",s->size);
+  }
+}
+// s size top
+char pop(StackPtr s){
+  NodePtr t;
+  int value;
+  t = s->top;
+  value = t->data; //get data inside the box b4 delete
+  s->top = t->nextPtr; //change top pointer to next box
+  free(t);
+  s->size -= 1;
+  return value;
+}
+
+void pop_all(StackPtr s){
+  NodePtr t = s->top;
+  char value;
+  while(s->top){
+    value = t->data;
+    s->top = t->nextPtr;
+    printf("Popping %c \n",value);
+    free(t);
+    t = s->top;
+    s->size -= 1;
+  }
+}
+
+
 #endif
